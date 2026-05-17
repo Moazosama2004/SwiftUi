48c14d17da6c7c24755dc6994df73514a205106e
Moaz Osama <moazosama204@gmail.com>
2026-05-17 05:25:37 -0700

    Initial Commit

---
+++ SwiftUiForBeginners/SwiftUiForBeginnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiForBeginnersApp.swift
+//  SwiftUiForBeginners
+//
+//  Created by Moaz on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiForBeginnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
